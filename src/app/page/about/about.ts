c3f920258929e17579d6b7380e88b2a6ba6d6e53
jackjd582 <jackjd582@gmail.com>
2026-07-08 15:30:17 +0530

    Design Changes

--- src/app/page/about/about.ts
+++ src/app/page/about/about.ts
@@ -10,22 +10,20 @@ import { CommonModule } from '@angular/common';
 })
 export class About {
 
-  founders = [
-    {
-      name: 'John Carter',
-      role: 'Founder',
-      image: 'assets/images/founder.avif',
-      desc:
-        'Focused on delivering premium car care products with innovation and long-lasting quality.'
-    },
-    {
-      name: 'Michael Roy',
-      role: 'Co-Founder',
-      image: 'assets/images/co-founder.avif',
-      desc:
-        'Passionate about customer experience and building trusted automotive care solutions.'
-    }
-  ];
+ founders = [
+  {
+    name: 'Pankaj Vataliya',
+    role: 'Founder & Product Director',
+    image: 'assets/images/founder.png',
+    desc: 'Leads product research, formulation, manufacturing, and quality assurance to ensure every We3 Car Care product delivers premium performance, long-lasting protection, and professional-grade results.'
+  },
+  {
+    name: 'Anurag Soni',
+    role: 'Co-Founder & Sales Director',
+    image: 'assets/images/co-founder.png',
+    desc: 'Heads sales strategy, business development, dealer partnerships, and customer relationships, driving market growth while ensuring exceptional service across every customer touchpoint.'
+  }
+];
 
   getImageUrl(path: string): string {
     return path;
